476700c51d25634ad8fd82475c935f90fd04e008
Steve Witman <stevewitman@gmail.com>
2021-11-17 23:23:11 -0700

    add firebase and emulators
      - add firebase 9.4.0 and angularfire 7.2.0
      - setup firebase configuration with environment for dev and prod
      - add getPosts method to data service to verify connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  useEmulators: false,
+  firebase: {
+    projectId: 'angularmentorsdotcom',
+    appId: '1:853298353874:web:a8322fd4a1848487b73f0b',
+    storageBucket: 'angularmentorsdotcom.appspot.com',
+    apiKey: 'AIzaSyBnkGwoYMu_yBWU8l0RS6Mwi6uvs6Y4W1s',
+    authDomain: 'angularmentorsdotcom.firebaseapp.com',
+    messagingSenderId: '853298353874',
+    measurementId: 'G-GPPCMMV9NV',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  useEmulators: true,
+  firebase: {
+    projectId: 'angularmentorsdotcom',
+    appId: '1:853298353874:web:a8322fd4a1848487b73f0b',
+    storageBucket: 'angularmentorsdotcom.appspot.com',
+    apiKey: 'AIzaSyBnkGwoYMu_yBWU8l0RS6Mwi6uvs6Y4W1s',
+    authDomain: 'angularmentorsdotcom.firebaseapp.com',
+    messagingSenderId: '853298353874',
+    measurementId: 'G-GPPCMMV9NV',
+  },
 };
 
 /*
